77d6915d1351d8b751e0ea4f473a31fa15aa2f34
Dragokillfist <tinker198431@gmail.com>
2025-01-25 20:20:19 -0600

    Fixed Yawgmoth
    - Fixed a crash that could occur when playing and scoring a card with the "Yawgmoth" enhancement when not having any cards left in hand to destroy.

--- Items/enha.lua
+++ Items/enha.lua
@@ -293,7 +293,7 @@ yawgmoth = SMODS.Enhancement {
                 trigger = 'after',
                 delay = 0.15,
                 func = function()
-                    if #G.hand.cards then
+                    if #G.hand.cards and #G.hand.cards >= 1 then
                         local temp_ID = G.hand.cards[1].base.id
                         local smallest = G.hand.cards[1]
                         for i=1, #G.hand.cards do
